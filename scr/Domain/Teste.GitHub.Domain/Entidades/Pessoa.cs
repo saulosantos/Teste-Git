875e0c1946d3db96a0a1a141cc22e107797ad314
Saulo-TI <saulosantos89@gmail.com>
2017-02-02 10:39:40 -0500

    Teste Commit

---
+++ scr/Domain/Teste.GitHub.Domain/Entidades/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.GitHub.Domain.Entidades
+{
+   public class Pessoa
+    {
+        public Pessoa()
+        {
+            this.propriedades = new List<Propriedade>();
+        }
+        
+
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public virtual ICollection<Propriedade> propriedades { get; set; }
+
+    }
+}
---
+++ scr/Domain/Teste.GitHub.Domain/Entidades/Propriedade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.GitHub.Domain.Entidades
+{
+   public  class Propriedade
+    {
+        public int PropriedadeId { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public int PessoaId { get; set; }
+
+        public virtual Pessoa Pessoa { get; set; }
+    }
+}
