5a1ec1abf09387d5e69e04354b8ea911dc485682
Saulo-TI <saulosantos89@gmail.com>
2017-02-02 16:24:36 -0500

    Antes criacao Data Migration

---
+++ Teste.GitHub.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Teste.GitHub.Web.MvcApplication" Language="C#" %>
---
+++ scr/Domain/Teste.GitHub.Domain/Context/DbContextGit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teste.GitHub.Domain.Entidades;
+
+namespace Teste.GitHub.Domain.Context
+{
+   public class DbContextGit : DbContext
+    {
+
+        public DbContextGit() : base("DbContextGit")
+        {
+                
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Propriedade> propriedades { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToOneConstraintIntroductionConvention>();
+            modelBuilder.Entity<Pessoa>().ToTable("Pessoas");
+            modelBuilder.Entity<Propriedade>().ToTable("Propriedades");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
