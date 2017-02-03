e93297852a458d3563ceac8cdc329ba7a00593ae
Saulo Santos <Saulo Santos@DESKTOP-QSMAOFG>
2017-02-02 23:26:28 -0500

    Teste com Data Annotetions.

--- scr/Domain/Teste.GitHub.Domain/Entidades/Pessoa.cs
+++ scr/Domain/Teste.GitHub.Domain/Entidades/Pessoa.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,31 @@ namespace Teste.GitHub.Domain.Entidades
             this.propriedades = new List<Propriedade>();
         }
         
-
+        [Key]
         public int PessoaId { get; set; }
+
+        //AllowEmptyStrings permite escrita em branco
+        [Required(ErrorMessage ="Informe o nome", AllowEmptyStrings=false)]
+        [MaxLength(100), MinLength(2, ErrorMessage ="Escreva no minimo 3 caracteres")]
         public string Nome { get; set; }
+
+        [MaxLength(100), MinLength(4, ErrorMessage = "Digite no minimo 4 caracteres")]
         public string Endereco { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o seu email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Data de nascimento obrigatorio", AllowEmptyStrings = false)]
+        [Display(Name = "Data Nascimento")]
+        [DisplayFormat(DataFormatString = "dd/mm/yyyy")]
         public DateTime DataNascimento { get; set; }
+
+
+        public string Telefone { get; set; }
+
         public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set; }
 
--- scr/Domain/Teste.GitHub.Domain/Entidades/Propriedade.cs
+++ scr/Domain/Teste.GitHub.Domain/Entidades/Propriedade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,21 @@ namespace Teste.GitHub.Domain.Entidades
 {
    public  class Propriedade
     {
+        [Key]
         public int PropriedadeId { get; set; }
+
+        [Required(ErrorMessage ="Digite o nome da propriedade")]
+        [MaxLength(100), MinLength(2)]
         public string Nome { get; set; }
+
+        [MaxLength(100), MinLength(4)]
         public string Endereco { get; set; }
         public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set; }
 
+
         public int PessoaId { get; set; }
+
 
         public virtual Pessoa Pessoa { get; set; }
     }
