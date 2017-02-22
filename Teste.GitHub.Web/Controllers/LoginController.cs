5ce1a5f216a0bff80190cc1dd5212794d4bf7fed
Saulo-TI <saulosantos89@gmail.com>
2017-02-22 17:04:20 -0500

    View Login com erro.

--- Teste.GitHub.Web/Controllers/LoginController.cs
+++ Teste.GitHub.Web/Controllers/LoginController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Teste.GitHub.Domain.Entidades;
+using Teste.GitHub.Domain.Repositorio;
 
 namespace Teste.GitHub.Web.Controllers
 {
     public class LoginController : Controller
     {
+        UsuarioRepositorio _contexto = new UsuarioRepositorio();
+
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+
+            if (ModelState.IsValid)
+            {
+              Usuario user =  _contexto.verificaUsuario(usuario);
+                if (user != null)
+                {
+                    if (!Equals(usuario.SenhaUsuario, user.SenhaUsuario))
+                    {
+                        ModelState.AddModelError("", "Senha/Usuario incorreta!");
+                    }
+                    else
+                    {
+                        FormsAuthentication.SetAuthCookie(user.LoginUser, false);
+                    }
+
+                    return RedirectToAction("Index", "home");
+                }
+            }
+
+
+            return View(new Usuario());
         }
     }
 }
